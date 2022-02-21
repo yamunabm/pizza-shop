e1d80e0962982dc4e5edc866c80cfc1ff7555357
ymallikarjuna <yamubm@gmail.com>
2022-02-22 00:07:35 +0530

    InitialCommit-framework dependencies
    InitialCommit:
    framework dependencies
    ER diagram
    Architecture

---
+++ pizza-shop/src/main/java/com/otto/catfish/task/pizza/PizzaShoppingApplication.java
@@ -0,0 +1,11 @@
+package com.otto.catfish.task.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaShoppingApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaShoppingApplication.class, args);
+	}
+}
